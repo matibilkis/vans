0a4903a4ff3c13a7b560dc98583ef201ef05dbd1
Arthur Pesah <arthur.pesah@gmail.com>
2020-06-26 10:29:44 +0200

    More printing from the environment

--- vans_gym/envs/vans_env.py
+++ vans_gym/envs/vans_env.py
@@ -23,10 +23,12 @@ class VansEnv(gym.Env):
 
 
         self.reward_history = np.array([])
+        self.history_final_reward = np.array([])
         self.quantum_state = np.array([0. for _ in range(2**self.n_qubits)])
         self.fidelity = np.inf
 
         self.episode = -1
+        self.i_step = 0
 
     def reset(self):
         """
@@ -36,19 +38,21 @@ class VansEnv(gym.Env):
         it should be enough to give the sequence of gates done.
         """
         self.episode += 1
+        self.i_step = 0
         self.state_indexed = np.array([])
         self.reward_history = np.array([])
         self.quantum_state = np.array([0. for _ in range(2 ** self.n_qubits)])
-
-        print("==================== Episode {} ====================".format(self.episode))
 
         return self.quantum_state
 
     def check_if_finish(self, reward):
         # np.count_nonzero(self.state_indexed,self.alphabet.CNOTS_indexes)
-        return len(self.state_indexed) > self.maximum_number_of_gates or reward == 1
+        return len(self.state_indexed) > self.maximum_number_of_gates \
+               or reward == 1 \
+               or (len(self.state_indexed) > 1 and self.state_indexed[-2] == self.state_indexed[-1])
 
     def step(self, action):
+        self.i_step += 1
         self.state_indexed = np.append(self.state_indexed, action)
 
         self.fidelity, self.quantum_state = self.solver.run_circuit(self.state_indexed)
@@ -59,14 +63,24 @@ class VansEnv(gym.Env):
 
         done = self.check_if_finish(reward)
 
+        if not done:
+            reward = 0
+
         if done:
-            print("List gates", self.state_indexed)
-            print("Reward", reward)
+            self.history_final_reward = np.append(self.history_final_reward, reward)
+            if self.episode % 50 == 0:
+                print("\n==================== Episode {} ====================\n".format(self.episode))
+                print("List gates", self.state_indexed)
+                print("Reward", reward)
+                print("Mean reward 100 episodes", np.mean(self.history_final_reward[-100:]))
 
         return self.quantum_state, reward, done, info
 
     def reward(self):
-        return self.fidelity
+        r = self.fidelity
+        if len(self.state_indexed) >= 2 and self.state_indexed[-1] == self.state_indexed[-2]:
+            r = -1
+        return r
 
     def render(self, mode="human"):
         fig, ax = plt.subplots(1, 1)
