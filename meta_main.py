9cd12ec06776c307d65dce351ce64df2653f0217
matibilkis <matiasbilkis@gmail.com>
2020-10-24 13:54:34 +0200

    first results noisy xxz depolarizing fixed J

--- meta_main.py
+++ meta_main.py
@@ -8,14 +8,15 @@ def channel_dict(channel="depolarizing", channel_params=[0], q_batch_size=10**2)
 
 nq = 3
 qeps = 10
-J = np.linspace(0,10,20)[6]
 
 insts=[]
 for p in [10**-7, 10**-6, 10**-5, 10**-4]:
-    noise_model = channel_dict(channel_params=[p], q_batch_size=10**3)
-    instruction = "python3 main.py --J "+str(J) + " --n_qubits "+str(nq)+" --reps "+str(50)+" --qepochs "+str(qeps)+ " --g "+str(1) + " --problem xxz --qlr 0.005" +" --noise_model "+noise_model + " --verbose 0"
-    # os.system(instruction)
-    insts.append(instruction)
+    for J in np.linspace(0,10,20):
+
+        noise_model = channel_dict(channel_params=[p], q_batch_size=10**3)
+        instruction = "python3 main.py --J "+str(J) + " --n_qubits "+str(nq)+" --reps "+str(50)+" --qepochs "+str(qeps)+ " --g "+str(1) + " --problem xxz --qlr 0.005" +" --noise_model "+noise_model + " --verbose 0"
+        # os.system(instruction)
+        insts.append(instruction)
 
 def execute_instruction(inst):
     os.system(inst)
