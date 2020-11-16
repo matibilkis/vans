abfdcd7562e81b40a2f8f79ec1afd47e2b6d70f9
matibilkis <matiasbilkis@gmail.com>
2020-11-16 13:06:43 +0100

    fo

--- meta_main.py
+++ meta_main.py
@@ -6,18 +6,18 @@ def channel_dict(channel="depolarizing", channel_params=[0], q_batch_size=10**2)
     d= '{\"channel\":\"' +channel + '\",\"channel_params\":'+str(channel_params)+',\"q_batch_size\":' + str(q_batch_size) + '}'
     return "\'"+d+ "\'"
 
-nq = 3
-qeps = 10
-
+nq = 10
+qeps = 10**4
+genetic_runs=200
 insts=[]
 # for p in [10**-7, 10**-6, 10**-5, 10**-4]:
-for p in [10**-8, 10**-3, 10**-2, 10**-1]:
-    for J in np.linspace(0,10,20):
+# for p in [10**-8, 10**-3, 10**-2, 10**-1]:
+for J in np.linspace(0,10,4):
 
-        noise_model = channel_dict(channel_params=[p], q_batch_size=10**3)
-        instruction = "python3 main.py --J "+str(J) + " --n_qubits "+str(nq)+" --reps "+str(50)+" --qepochs "+str(qeps)+ " --g "+str(1) + " --problem TFIM --qlr 0.005" +" --noise_model "+noise_model + " --verbose 0"
-        # os.system(instruction)
-        insts.append(instruction)
+    # noise_model = channel_dict(channel_params=[p], q_batch_size=10**3)
+    instruction = "python3 main.py --J "+str(J) + " --n_qubits "+str(nq)+" --reps "+str(genetic_runs)+" --qepochs "+str(qeps)+ " --g "+str(1) + " --problem xxz --qlr 0.005" #+" --noise_model "+noise_model + " --verbose 0"
+    # os.system(instruction)
+    insts.append(instruction)
 
 def execute_instruction(inst):
     os.system(inst)
