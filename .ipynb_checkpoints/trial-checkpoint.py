89b5ed756168d769ff91714bbe061b192c46c016
matias.bilkis@uab.cat <mbilkis@tds249.pic.es>
2021-02-20 00:46:51 +0100

    mate

--- .ipynb_checkpoints/trial-checkpoint.py
+++ .ipynb_checkpoints/trial-checkpoint.py
@@ -4,6 +4,7 @@ import os
 bd=2
 problem_config = dict_to_json({"problem" : "H4", "geometry": [('H', (0., 0., 0.)), ('H', (0., 0., bd)), ('H', (0., 0., 2*bd)), ('H', (0., 0., 3*bd))], "multiplicity":1, "charge":0, "basis":"sto-3g"});QUBITS = 8
 
-st = "python3 main.py --path_results \"/data/uab-giq/scratch/matias/data-vans/\" --qlr 0.01 --acceptange_percentage 0.01 --n_qubits {} --reps 1000 --qepochs 1000 --problem_config {} --optimizer sgd".format(QUBITS,problem_config)
+st = "python3 main.py --path_results \"/data/uab-giq/scratch/matias/data-vans/\" --qlr 0.01 --acceptange_percentage 0.1 --n_qubits {} --reps 1000 --qepochs 1000 --problem_config {} --optimizer sgd".format(QUBITS,problem_config)
 
-os.system(st)
+print(st)
+#os.system(st)
--- pic_submit.sh
+++ pic_submit.sh
@@ -1,5 +1,5 @@
 amplitude=$1
 . ~/qenv_bilkis/bin/activate
 cd ~/vans
-python3 main.py --Nphases 2 --amplitude $amplitude --Npriors 200 --photodetectors 8
+python3 main.py --path_results "/data/uab-giq/scratch/matias/data-vans/" --qlr 0.01 --acceptange_percentage 0.1 --n_qubits 8 --reps 1000 --qepochs 1000 --problem_config '{"problem":"H4","geometry":"[('H', (0.0, 0.0, 0.0)), ('H', (0.0, 0.0, 2)), ('H', (0.0, 0.0, 4)), ('H', (0.0, 0.0, 6))]","multiplicity":"1","charge":"0","basis":"sto-3g"}' --optimizer sgd
 deactivate
