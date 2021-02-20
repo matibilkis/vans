f117f72133bae7d556c7695f75fe55b74e6c99bb
matias.bilkis@uab.cat <mbilkis@td098.pic.es>
2021-02-20 05:35:03 +0100

    addding n gates per step with some exp decaying probability

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -36,8 +36,9 @@ if __name__ == "__main__":
     parser.add_argument("--problem_config", type=json.loads, default='{}')
     parser.add_argument("--noise_config", type=json.loads, default='{}')
     parser.add_argument("--acceptange_percentage", type=float, default=0.01)
-    parser.add_argument("--accuracy_to_end", type=float, default=-np.inf)
+    parser.add_argument("--return_lower_bound", type=int, default=0)
     parser.add_argument("--show_tensorboarddata",type=int, default=0)
+    parser.add_argument("--rate_iids_per_step",type=float,default=1)
     args = parser.parse_args()
 
     begin = datetime.now()
@@ -46,9 +47,13 @@ if __name__ == "__main__":
     #VQE module, in charge of continuous optimization
     vqe_handler = VQE(n_qubits=args.n_qubits, lr=args.qlr, epochs=args.qepochs, verbose=args.verbose,
                         noise_config=args.noise_config, problem_config=args.problem_config,
-                        patience=args.training_patience, random_perturbations=True)
+                        patience=args.training_patience, random_perturbations=True, return_lower_bound=[True, False][args.return_lower_bound], optimizer=args.optimizer)
 
     start = datetime.now()
+    
+    
+    #info = "len(n_qubits):{}\nnoise: {}\nqlr: {}\nqepochs: {}\npatience: {}\ngenetic runs: {}\nacceptange_percentage runs:{}\nproblem_info:{}\n".format(vqe_handler.n_qubits,args.noise_config,vqe_handler.lr,vqe_handler.epochs,vqe_handler.patience,args.reps,args.acceptange_percentage,args.problem_config)
+    
     info = f"len(n_qubits): {vqe_handler.n_qubits}\n" \
                         f"noise: {args.noise_config}\n"\
                         f"qlr: {vqe_handler.lr}\n" \
@@ -57,32 +62,42 @@ if __name__ == "__main__":
                         f"genetic runs: {args.reps}\n" \
                         f"acceptange_percentage runs: {args.acceptange_percentage}\n" \
                         f"problem_info: {args.problem_config}\n"
-
+    
     #Evaluator keeps a record of the circuit and accepts or not certain configuration
-    evaluator = Evaluator(vars(args), info=info, path=args.path_results, acceptange_percentage=args.acceptange_percentage, accuracy_to_end=args.accuracy_to_end)
+    evaluator = Evaluator(vars(args), info=info, path=args.path_results, acceptange_percentage=args.acceptange_percentage, accuracy_to_end=vqe_handler.lower_bound_energy)
     evaluator.displaying +=info
 
     if args.show_tensorboarddata == 1:
         vqe_handler.tensorboarddata = evaluator.directory
     #IdInserter appends to a given circuit an identity resolution
-    iid = IdInserter(n_qubits=args.n_qubits)
+    iid = IdInserter(n_qubits=len(vqe_handler.qubits))
 
     #Simplifier reduces gates number as much as possible while keeping same expected value of target hamiltonian
-    Simp = Simplifier(n_qubits=args.n_qubits)
+    Simp = Simplifier(n_qubits=len(vqe_handler.qubits))
 
     #UnitaryMuerder is in charge of evaluating changes on the energy while setting apart one (or more) parametrized gates. If
     killer = UnitaryMurder(vqe_handler, noise_config=args.noise_config)
 
     ### begin with a product ansatz
     indexed_circuit=[vqe_handler.number_of_cnots+k for k in range(vqe_handler.n_qubits,2*vqe_handler.n_qubits)]
+
+    ### add some no local gates..
+    for i in range(len(vqe_handler.qubits)):
+        indexed_circuit+=iid.resolution_2cnots(i,(i+1)%len(vqe_handler.qubits))
+    indexed_circuit+=[vqe_handler.number_of_cnots+k for k in range(vqe_handler.n_qubits,2*vqe_handler.n_qubits)]
+
     energy, symbol_to_value, training_evolution = vqe_handler.vqe(indexed_circuit) #compute energy
 
     #add initial info to evaluator
 
     to_print="\nIteration #{}\nTime since beggining:{}\n ".format(0, datetime.now()-start)+str("**"*20)+"\n"+str(str("*"*20))+"\ncurrent energy: {}\n\n{}\n\n".format(energy,vqe_handler.give_unitary(indexed_circuit,symbol_to_value))
     to_print+="\n\n"
-    print(to_print)
-    evaluator.displaying+=to_print
+    
+    print(energy)
+    #print(vqe_handler.give_unitary(indexed_circuit,symbol_to_value).encode('utf-8'))
+    print("\n\n")
+    #print(to_print)
+    #evaluator.displaying+=to_print
 
 
     evaluator.add_step(indexed_circuit, symbol_to_value, energy, relevant=True)
@@ -90,8 +105,10 @@ if __name__ == "__main__":
 
     for iteration in range(args.reps):
         relevant=False
-        ### create a mutation M (maybe this word is too fancy)
-        M_indices, M_symbols_to_values, M_idx_to_symbols = iid.place_almost_identity(indexed_circuit, symbol_to_value)
+        
+        
+        ### create a mutation M (maybe this word is too fancy); we add (probably more than one) identity resolution
+        M_indices, M_symbols_to_values, M_idx_to_symbols = iid.place_identities(indexed_circuit, symbol_to_value, rate_iids_per_step= args.rate_iids_per_step)
 
         ### simplify the circuit as much as possible
         Sindices, Ssymbols_to_values, Sindex_to_symbols = Simp.reduce_circuit(M_indices, M_symbols_to_values, M_idx_to_symbols)
@@ -112,10 +129,12 @@ if __name__ == "__main__":
             relevant=True
         evaluator.add_step(indexed_circuit, symbol_to_value, energy, relevant=relevant)
 
-        to_print="\nIteration #{}\nTime since beggining:{}\n ".format(iteration, datetime.now()-start)+str("**"*20)+"\n"+str(str("*"*20))+"\ncurrent energy: {}\n\n{}\n\n".format(energy,vqe_handler.give_unitary(indexed_circuit,symbol_to_value))
-        to_print+="\n\n"
-        print(to_print)
-        evaluator.displaying +=to_print
+        to_print="\nIteration #{}\nTime since beggining:{}\n best energy: {}\n lower_bound: {}".format(iteration, start-datetime.now(), evaluator.lowest_energy, evaluator.accuracy_to_end)
+        
+        #+str("**"*20)+"\n"+str(str("*"*20))+"\ncurrent energy: {}\n\n{}\n\n".format(energy,vqe_handler.give_unitary(indexed_circuit,symbol_to_value))
+        #to_print+="\n\n"
+        #print(to_print)
+        #evaluator.displaying +=to_print
 
         ## save results of iteration.
         evaluator.save_dicts_and_displaying()
