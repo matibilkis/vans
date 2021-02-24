dcb5b8b0bcdb6f1c53e7b0703e62eed514dfff44
matibilkis <matiasbilkis@gmail.com>
2021-02-24 19:21:49 +0100

    pulling from barna

--- simulate_bash.py
+++ simulate_bash.py
@@ -7,6 +7,7 @@ parser.add_argument("--bonds", type=float, default=1.5)
 parser.add_argument("--ratesiid", type=float, default=1.)
 parser.add_argument("--nrun", type=float, default=1.)
 parser.add_argument("--J", type=float, default=2.)
+parser.add_argument("--optimizer", type=str, default="adam")
 
 args = parser.parse_args()
 bond=args.bonds
@@ -27,5 +28,5 @@ problem_config = dict_to_json({"problem" : "XXZ", "g":1.0, "J": args.J});q=12
 ### POSSIBLE PATHS
 # path="/data/uab-giq/scratch/matias/data-vans/"
 path = "../data-vans/"
-st = "python3 main.py --path_results \"{}\" --qlr 0.01 --acceptance_percentage 0.001 --n_qubits {} --reps 2000 --qepochs 2000 --problem_config {} --show_tensorboarddata 0 --optimizer sgd --training_patience 200 --rate_iids_per_step 1.0 --specific_name __sgd__ --wait_to_get_back 2".format(path,q,problem_config)
+st = "python3 main.py --path_results \"{}\" --qlr 0.01 --acceptance_percentage 0.001 --n_qubits {} --reps 2000 --qepochs 2000 --problem_config {} --show_tensorboarddata 0 --optimizer {} --training_patience 200 --rate_iids_per_step 1.0 --specific_name __{}__ --wait_to_get_back 2".format(path,q,problem_config, args.optimizer, args.optimizer)
 os.system(st)
